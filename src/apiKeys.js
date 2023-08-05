cbeafdf8d3001316716c3a21788ec121486a11f2
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-08-05 14:05:02 +0530

    src

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "3d5e741248975c059cd6b22648075cb8",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
