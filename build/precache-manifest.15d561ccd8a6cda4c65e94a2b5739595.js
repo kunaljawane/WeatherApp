7fcecdda21ec45246c0333313a8f5fc786fdbd5a
KUNAL JAWANE <89718770+kunaljawane@users.noreply.github.com>
2023-08-07 12:27:44 +0530

    responsivness

---
+++ build/precache-manifest.15d561ccd8a6cda4c65e94a2b5739595.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d3f978f32a4275acbaaf8a5d1361ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "108b8ee576d2d2db2664",
+    "url": "/static/css/main.08fff072.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "108b8ee576d2d2db2664",
+    "url": "/static/js/main.be8b3e19.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "ef88176b7dccfa6951abda0c1c9b2600",
+    "url": "/static/media/bg.ef88176b.jpg"
+  },
+  {
+    "revision": "87a40cef8d7b4b6167171f51b7492a74",
+    "url": "/static/media/city.87a40cef.jpg"
+  }
+]);
